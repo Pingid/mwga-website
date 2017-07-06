88867180991023e019d19db6a54b19083c68f1ba
Daniel Beaven <dan@Daniels-MacBook-Air.local>
2017-07-06 17:16:45 +0100

    minor css changes

--- src/App.js
+++ src/App.js
@@ -11,11 +11,14 @@ class App extends Component {
     return (
       <div>
         <Header />
-        <div className="py3">
+        <div className="pt3">
           <a className="caps text-decoration-none center p2 get-notified nowrap" style={{ width: '100%', background: 'white' }} href="http://eepurl.com/cVc6cj">Get notified</a>
           <h4 className="caps mt3">when the game is released</h4>
         </div>
         <About />
+        <div>
+
+        </div>
       </div>
     );
   }
--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Header = () => (
   <div className="pt1">
     <div className="header">
-      <h1 className="caps m0">Make <br/>the <br/>world <br/>great <br/>again</h1>
-      <h4 className="caps m0 mt1" style={{ width: '100%' }}>The card game</h4>
+      <h1 className="caps m0 title">Make the world <br/>great again</h1>
+      <h3 className="caps m0 mt1 sub-title" style={{ width: '100%' }}>The card game</h3>
     </div>
   </div>
 );
